5f1f733349d8d04d1ac16e72b928fdedf3315033
Stephen-Baxter <52463998+Stephen-Baxter@users.noreply.github.com>
2019-09-08 19:55:06 -0500

    Update _.js

--- _.js
+++ _.js
@@ -6,7 +6,12 @@ window.onload = function ()
 {
 	tab = [[0, 0], [0, 0, 0, 0], [0, 0, 0]];
 
-	programList = data;
+	programList = [
+	[[[1,0], [1,0,0,0], [1,0,0]], "Website", "https://github.com/Stephen-Baxter"],
+	[[[0,1], [0,1,0,0], [0,1,0]], "RPG Dice", "https://github.com/Stephen-Baxter/CppProjects/tree/master/CLI/RPG%20Dice"],
+	[[[0,1], [0,0,1,0], [0,1,0]], "RPG Dice", "https://github.com/Stephen-Baxter/CsProjects/tree/master/CLI/RPG%20Dice"],
+	[[[0,1], [0,0,0,1], [0,1,0]], "RPG Dice", "https://github.com/Stephen-Baxter/VB.netProjects/tree/master/CLI/RPG%20Dice"]
+];
 
 	document.getElementById("df2").click();
 	document.getElementById("df3").click();
