b5391290c2294d02e3682af8aed61523de149401
Stephen Baxter (Nickname: Jak E Chronicle) <52463998+Stephen-Baxter@users.noreply.github.com>
2022-05-26 20:42:16 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,89 @@
+///<reference path="C:\github\javascript-sdk\jquery\jquery-3.6.0.min.js"></reference>
+let currentPageNumber = 0;
+let degreesTurned = 0;
+let menuInMobileToggleOn = false;
+
 const ChangeMainPage = function(page_number_)
 {
-    window.console.log(page_number_);
-    
+    $("#MAIN_").children().hide(); $("#MAIN_").children().css("transform", "rotateY(" + (degreesTurned % 360) + "deg)");
+    if (currentPageNumber === page_number_)
+    {
+        $("#MAIN_").children().eq(page_number_).show(); $("#MAIN_").children().eq(page_number_).css("transform", "rotateY(" + (degreesTurned % 360) + "deg)");
+    }
+    else
+    {
+        $("#MENU_").children().eq(currentPageNumber).children().eq(0).css("transform", "rotateZ(0deg)");
+        $("#MENU_").children().eq(page_number_).children().eq(0).css("transform", "rotateZ(360deg)");
+        $("#MENU_").children().eq(currentPageNumber).removeClass("SELECTED_");
+        $("#MENU_").children().eq(page_number_).addClass("SELECTED_");
+        degreesTurned = (currentPageNumber < page_number_) ? (degreesTurned + 180) : (degreesTurned - 180);
+        $("#MAIN_").children().eq(currentPageNumber).show(); $("#MAIN_").children().eq(currentPageNumber).css("transform", "rotateY(" + ((degreesTurned % 360) + 180) + "deg)");
+        $("#MAIN_").children().eq(page_number_).show(); $("#MAIN_").children().eq(page_number_).css("transform", "rotateY(" + (degreesTurned % 360) + "deg)");
+        $("#MAIN_").css("transform", "rotateY(" + degreesTurned + "deg)");  
+        currentPageNumber = page_number_; 
+        if ($(window).width() < $(window).height())
+        {
+            $("#MENU_MOBILE_DROPDOWN_BUTTON_").css("transform", "rotateZ(0deg)");
+            $("#MENU_").fadeOut(400);
+            menuInMobileToggleOn = !menuInMobileToggleOn;
+        }
+    }
+}
+
+const ToggleMenuInMobile = function()
+{
+    menuInMobileToggleOn = !menuInMobileToggleOn;
+    if (menuInMobileToggleOn)
+    {
+        $("#MENU_MOBILE_DROPDOWN_BUTTON_").css("transform", "rotateZ(90deg)");
+        $("#MENU_").fadeIn(400);
+    }
+    else
+    {
+        $("#MENU_MOBILE_DROPDOWN_BUTTON_").css("transform", "rotateZ(0deg)");
+        $("#MENU_").fadeOut(400);
+    }
 }
 
 const OnResize = function()
 {
-    let windowWidth = $(window).width();
-    let windowHeight = $(window).height();
-    //window.console.log(windowWidth, windowHeight);
-
-    if (windowWidth >= windowHeight)
+    if ($(window).width() >= $(window).height())
     {
-        $("#MENU_DESKTOP_").show(); $("#MENU_MOBILE_").hide()
-        $("#LAYOUT_").css("grid-template-rows", "50px auto 20px")
-        $("#LAYOUT_").css("grid-template-areas", "'HEADER_ HEADER_ HEADER_' 'MENU_DESKTOP_ MAIN_ MAIN_' 'FOOTER_ FOOTER_ FOOTER_'")
+        menuInMobileToggleOn = false
+        $("#MENU_").show(); $("#MENU_MOBILE_DROPDOWN_BUTTON_").hide();
+        $("#LAYOUT_").css("grid-template-areas", "'HEADER_ HEADER_ HEADER_' 'MENU_ MAIN_ MAIN_' 'FOOTER_ FOOTER_ FOOTER_'")
     }
     else
     {
-        $("#MENU_DESKTOP_").hide(); $("#MENU_MOBILE_").show()
-        $("#LAYOUT_").css("grid-template-rows", "50px 50px auto 20px")
-        $("#LAYOUT_").css("grid-template-areas", "'HEADER_ HEADER_ HEADER_' 'MENU_MOBILE_ MENU_MOBILE_ MENU_MOBILE_' 'MAIN_ MAIN_ MAIN_' 'FOOTER_ FOOTER_ FOOTER_'")
+        $("#MENU_").hide(); $("#MENU_MOBILE_DROPDOWN_BUTTON_").fadeIn(250);
+        $("#LAYOUT_").css("grid-template-areas", "'HEADER_ HEADER_ HEADER_' 'MAIN_ MAIN_ MAIN_' 'FOOTER_ FOOTER_ FOOTER_'")
     }
+
+    if ($("#MAIN_").width() >= $("#MAIN_").height())
+    {
+        $("#HOME_PAGE_3D_EFECT_").height("100%");
+        $("#HOME_PAGE_3D_EFECT_").width($("#HOME_PAGE_3D_EFECT_").height());
+    }
+    else
+    {
+        $("#HOME_PAGE_3D_EFECT_").width("100%");
+        $("#HOME_PAGE_3D_EFECT_").height($("#HOME_PAGE_3D_EFECT_").width());
+    }
+
+    UpdateSceneSize()
 };
 
-const main = function()
+function main()
 {
-    OnResize()
-    window.console.log(123);
-};
+    main_HomePage();
+    OnResize();
+    let urlAtLoad = ""+ window.location.href;
+    if (urlAtLoad.endsWith('#AI_DEMONSTRATION_PAGE_')) currentPageNumber = 1;
+    else if (urlAtLoad.endsWith('#OTHER_PROJECTS_PAGE_')) currentPageNumber = 2;
+    else currentPageNumber = 0;
+    $("#MENU_").children().eq(currentPageNumber).addClass("SELECTED_");
+    $("#MENU_").children().eq(currentPageNumber).children().eq(0).css("transform", "rotateZ(360deg)");
+    ChangeMainPage(currentPageNumber);
 
-$(window).resize(OnResize);
-//$(window).on("load", main);
+    $(window).resize(OnResize);
+};
